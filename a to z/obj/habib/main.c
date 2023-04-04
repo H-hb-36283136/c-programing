dd5798c12d8d1fd89b83aa80496fa9e6b7162ff5
H-hb-36283136 <hhb32784044@gmail.com>
2023-04-04 11:08:14 +0600

    habib

---
+++ a to z/obj/habib/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("You need to print the lines given below in the same format as it is -\n");
+    printf("Recently I heard that you’ve achieved 95% marks in your exam. \n This is brilliant! \n");
+    printf("I wish you’ll shine in your life!\tGood luck with all the barriers(/\) in \n your life\n");
+}
